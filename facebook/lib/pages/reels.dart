2c31041fd1e483c30f07a6c96bd6764f70c2e9b9
github <119272168+Po0mi@users.noreply.github.com>
2025-09-30 12:33:58 +0800

    update

---
+++ facebook/lib/pages/reels.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ReelsPages extends StatefulWidget {
+  const ReelsPages({super.key});
+
+  @override
+  State<ReelsPages> createState() => _ReelsPagesState();
+}
+
+class _ReelsPagesState extends State<ReelsPages> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
